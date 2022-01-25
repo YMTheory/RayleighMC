c97f86b425926070c2e59817640919e1c8c8a3d3
YMTheory <932899789@qq.com>
2022-01-25 16:59:02 +0800

    add 1D, 2D and 3D option for plotting

--- draw_dist.py
+++ draw_dist.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator
+from matplotlib import cm
 
 import uproot as up
 from tqdm import tqdm
 
 if __name__ == "__main__" :
+
+    draw1D = False
+    draw2D = False
+    draw3D = True
+
 
     kx, ky, kz, px, py, pz = [], [], [], [], [], []
     for i in tqdm(range(100)):
@@ -30,7 +37,7 @@ if __name__ == "__main__" :
     kx = np.array(kx)
     ky = np.array(ky)
     kz = np.array(kz)
-    
+
     costheta = kz / np.sqrt(kx**2 + ky**2 + kz**2)
     phi = []
     for i, j in zip(kx, ky):
@@ -44,25 +51,74 @@ if __name__ == "__main__" :
         else:
             phi.append(2*np.pi - tmpphi)
 
-    #fig, (ax1, ax2) = plt.subplots(2, 1)
-    #ax1.hist(costheta, bins=100)
-    #ax1.set_xlabel(r"cos($\theta$)", fontsize=15)
-    #ax1.tick_params(axis='both', which='major', labelsize=15)
 
-    #ax2.hist(phi, bins=100)
-    #ax2.set_xlabel(r"$\phi$", fontsize=15)
-    #ax2.tick_params(axis='both', which='major', labelsize=15)
+    phi = np.array(phi)
+    cosbeta = 1*py 
 
-    fig, ax = plt.subplots()
-    h = ax.hist2d(costheta, phi, bins=(100, 100))
-    ax.set_xlabel(r"cos($\Theta$)", fontsize=15)
-    ax.set_ylabel(r"$\phi$", fontsize=15)
-    ax.tick_params(axis='both', which='major', labelsize=15)
-    fig.colorbar(h[3], ax=ax)
 
-    plt.tight_layout()
-    plt.savefig("./pdffiles/scatAng2D_inPol010_inMom001.pdf")
-    #plt.savefig("./pdffiles/scatAng_inPol010_inMom001.pdf")
+    if draw1D:
+
+        fig, (ax1, ax2) = plt.subplots(2, 1)
+        ax1.hist(costheta, bins=100)
+        ax1.set_xlabel(r"cos($\theta$)", fontsize=15)
+        ax1.tick_params(axis='both', which='major', labelsize=15)
+
+        ax2.hist(phi, bins=100)
+        ax2.set_xlabel(r"$\phi$", fontsize=15)
+        ax2.tick_params(axis='both', which='major', labelsize=15)
+        plt.tight_layout()
+        plt.savefig("./pdffiles/scatAng_inPol010_inMom001.pdf")
+
+    if draw2D:
+        fig, ax = plt.subplots()
+        h = ax.hist2d(costheta, phi, bins=(100, 100))
+        ax.set_xlabel(r"cos($\Theta$)", fontsize=15)
+        ax.set_ylabel(r"$\phi$", fontsize=15)
+        ax.tick_params(axis='both', which='major', labelsize=15)
+        fig.colorbar(h[3], ax=ax)
+
+        plt.tight_layout()
+        plt.savefig("./pdffiles/scatAng2D_inPol010_inMom001.pdf")
+
+    if draw3D:
+        count = np.zeros((100, 100))
+        value = np.zeros((100, 100))
+        binwX, binwY = 2./100., 2*np.pi/100
+        for i, j, k in zip(costheta, phi, cosbeta):
+            binNx, binNy = int((i+1)/binwX) , int(j/binwY)
+            count[binNx, binNy] += 1
+            value[binNx, binNy] += k
+
+        data = np.zeros((100, 100))
+        for i in range(100):
+            for j in range(100):
+                if count[i, j] == 0:
+                    data[i, j] = 0
+                else:
+                    data[i, j] = value[i, j] / count[i, j]
+
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        X, Y = np.meshgrid(np.linspace(-1, 1, num=100), np.linspace(0, 2*np.pi, num=100))
+        # Plot the surface.
+
+        surf = ax.plot_surface(X, Y, data, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+        
+        ax.set_xlabel(r"cos($\theta$)", fontsize=13)
+        ax.set_ylabel(r"$\phi$", fontsize=13)
+        ax.set_zlabel(r"cos($\beta$)", fontsize=13)
+
+        # Customize the z axis.
+        #ax.set_zlim(-1.01, 1.01)
+        #ax.zaxis.set_major_locator(LinearLocator(10))
+        # A StrMethodFormatter is used automatically
+        #ax.zaxis.set_major_formatter('{x:.02f}')
+
+        # Add a color bar which maps values to colors.
+        plt.tight_layout()
+        plt.savefig("./pdffiles/scatAng3D_inPol010_inMom001.pdf")
+    
     plt.show()
 
 
