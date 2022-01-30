5c7aade4d0a489c55a2bbe03cda032c11c22735f
YMTheory <932899789@qq.com>
2022-01-30 16:41:44 +0800

    draw polar plots for 7 theta deg

--- analyser.py
+++ analyser.py
@@ -4,9 +4,9 @@ import numpy as np
 def func(p0, p1, p2, x) :
     return p0 + p1*np.cos(x-p2)*np.cos(x-p2)
 
-def read_data(theta):
+def read_data(theta, pol):
     vec_pol_angle = [179, 209, 239, 269, 299, 329, 359, 29, 59, 89, 119, 149]
-    data = np.loadtxt("./exp/ver"+str(int(theta))+"_1.txt")
+    data = np.loadtxt("./exp/"+pol+str(int(theta))+"_1.txt")
 
     R    = data[:, 0]
     Rerr = data[:, 1]
@@ -71,4 +71,63 @@ def analyse_verData():
     return Hv, Hverr, Vv, Vverr, rhov, rhov_err
 
 
+
+def analyse_horData():
+
+    theta = [75, 80, 85, 90, 95, 100, 105]
+    vec_pol_angle = [179, 209, 239, 269, 299, 329, 359, 29, 59, 89, 119, 149]
+
+    rhoh, rhoh_err = [], []
+    Vh, Vherr, Hh, Hherr = [], [], [], []
+
+    vec_pol_angle = [179, 209, 239, 269, 299, 329, 359, 29, 59, 89, 119, 149]
+    for n, i in enumerate(theta):
+
+        data = np.loadtxt("./exp/hor"+str(i)+"_1.txt")
+
+        R    = data[:, 0]
+        Rerr = data[:, 1]
+
+        g1 = ROOT.TGraphErrors()
+        for k in range(len(vec_pol_angle)):
+            g1.SetPoint(k, vec_pol_angle[k]/180.*np.pi, R[k])
+            g1.SetPointError(k, 0, Rerr[k])
+
+        f1 = ROOT.TF1("f1", "[0] + [1]*cos(x-[2])*cos(x-[2])", 0, np.pi*2)
+        g1.Fit(f1, "RE")
+        p0, p1, p2 = f1.GetParameter(0), f1.GetParameter(1), f1.GetParameter(2)
+        p0err, p1err, p2err = f1.GetParError(0), f1.GetParError(1), f1.GetParError(2)
+
+        if p1 < 0:
+            Vh.append(p0+p1)
+            Vhmax = p0 + p1 + p0err + p1err
+            Vhmin = p0 + p1 - p0err - p1err
+            Vherr.append((Vhmax-Vhmin)/2.)
+
+            Hh.append(p0)
+            Hhmax = p0 + p0err
+            Hhmin = p0 - p0err
+            Hherr.append((Hhmax-Hhmin)/2.)
+
+            print("Vh = %.3f, Hh = %.3f" %(Vh[-1], Hh[-1]))
+    
+        if p1 >= 0:
+            Hh.append(p0+p1)
+            Hhmax = p0 + p1 + p0err + p1err
+            Hhmin = p0 + p1 - p0err - p1err
+            Hherr.append((Hhmax-Hhmin)/2.)
+
+            Vh.append(p0)
+            Vhmax = p0 + p0err
+            Vhmin = p0 - p0err
+            Vherr.append((Vhmax-Vhmin)/2.)
+
+            print("Vh = %.3f, Hh = %.3f" %(Vh[-1], Hh[-1]))
+
+        rhoh.append(Hh[-1]/Vh[-1])
+        rhoh_max = Hhmax / Vhmin
+        rhoh_min = Hhmin / Vhmax
+        rhoh_err.append((rhoh_max - rhoh_min)/2.)
+
+    return Hh, Hherr, Vh, Vherr, rhoh, rhoh_err
 
--- drawTool.py
+++ drawTool.py
@@ -31,26 +31,48 @@ if __name__ == "__main__" :
         fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
         #### experimental data
-        ang = 90.
+        ang = 100.
         vec_pol_angle = np.array([179, 209, 239, 269, 299, 329, 359, 29, 59, 89, 119, 149])
-        R, Rerr, p0, p1, p2, p0err, p1err, p2err = ana.read_data(ang)
+        R, Rerr, p0, p1, p2, p0err, p1err, p2err = ana.read_data(ang, "ver")
         dx = np.arange(0, 2*np.pi, 0.01)
         dy = []
         for i in dx:
-            dy.append(func(p0, p1, p2, i))
+            dy.append(func(p0, p1, 0, i))
         dy = np.array(dy)
         ## simulation
-        polAngle_arr, prob_arr = pt.generator(1, 0, 0, 0.20, 90.)
+        polAngle_arr, prob_arr = pt.generator(1, 0, 0, 0.208, ang)
 
-        norm = (prob_arr[90] + prob_arr[270]) / 2.
-        print("MC normalization factor = %.3e" %norm)
+        norm1 = (prob_arr[90] + prob_arr[270]) / 2.
+        print("MC normalization factor = %.3e" %norm1)
 
-        ax.plot(dx+p2, dy/p0, "--", lw=2, color="black", label="Fitting")
-        ax.plot(vec_pol_angle/180.*np.pi+p2, R/p0, "o", color="red", label="Exp")
-        ax.plot(polAngle_arr, prob_arr/norm, "-", lw=2, color="blue", label="MC")
+        #ax.plot(dx, dy/p0, "--", lw=2, color="black", label="Fitting")
+        ax.errorbar(vec_pol_angle/180.*np.pi-p2, R/p0, yerr=Rerr/p0, fmt="^", ms=7, fillstyle="none", color="red", label="Exp: vertical")
+        ax.plot(polAngle_arr, prob_arr/norm1, "-", lw=2, color="blue", label="MC: vertical")
+
+        # horizontal
+        R, Rerr, p0, p1, p2, p0err, p1err, p2err = ana.read_data(ang, "hor")
+        dx = np.arange(0, 2*np.pi, 0.01)
+        dy = []
+        for i in dx:
+            dy.append(func(p0, p1, 0, i))
+        dy = np.array(dy)
+        ## simulation
+        polAngle_arr, prob_arr = pt.generator(0, 1, 0, 0.208, ang)
+
+        norm2 = (prob_arr[0] + prob_arr[180]) / 2.
+        print("MC normalization factor = %.3e" %norm2)
+
+        #ax.plot(dx, dy/p0, "--", lw=2, color="black", label="Fitting")
+        ax.errorbar(vec_pol_angle/180.*np.pi-p2+np.pi/2, R/(p0), yerr=Rerr/(p0), fmt="v", ms=7, fillstyle="none", color="red", label="Exp: horizontal")
+        ax.plot(polAngle_arr, prob_arr/norm2, "-", lw=2, color="blue", label="MC: horizontal")
+
+
+        #ax.set_xlabel(r"$\beta$", fontsize=15)
         ax.set_theta_zero_location("N", np.pi/2.)
-        ax.legend()
-        ax.set_yticklabels([])
+        #ax.legend(bbox_to_anchor=(0.1, 0.3, 1, 1), ncol=2)
+        #ax.set_yticklabels([])
+
+        ax.text(200, 6, r"$\theta=%d$ deg"%ang, bbox=dict(facecolor='white', alpha=0.5), fontsize=15)
 
         plt.tight_layout()
         plt.savefig("./pdffiles/Polar_"+str(ang)+"deg.pdf")
