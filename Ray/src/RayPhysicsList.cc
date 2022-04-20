66c39d599a700976e6fcbf2986e1c7257cfe17ce
YMTheory <932899789@qq.com>
2022-04-20 14:47:56 +0800

    Register Livermore model as Em Physics

--- Ray/src/RayPhysicsList.cc
+++ Ray/src/RayPhysicsList.cc
@@ -4,23 +4,46 @@
  @File Name: RayPhysicsList.cc
  ************************************************************************/
 
+#include "G4ProcessManager.hh"
 #include "RayPhysicsList.hh"
 #include "RayleighScattering.hh"
+#include "G4EmLivermorePhysics.hh"
+#include "G4EmExtraPhysics.hh"
+#include "G4DecayPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+#include "G4HadronElasticPhysicsHP.hh"
+#include "G4HadronPhysicsQGSP_BERT_HP.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4IonPhysicsPHP.hh"
 
 #include "G4OpticalPhysics.hh"
 #include "G4SystemOfUnits.hh"
 
 // particles
+#include "G4LeptonConstructor.hh"
+#include "G4BosonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+
 #include "G4OpticalPhoton.hh"
-#include "G4ProcessManager.hh"
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
+
 
 RayPhysicsList::RayPhysicsList() : G4VModularPhysicsList()
 {
     defaultCutValue = 1.0*mm;
 
+    RegisterPhysics(new G4EmLivermorePhysics());
+
 }
 
-RayPhysicsList::~RayPhysicsList() {}
+RayPhysicsList::~RayPhysicsList() {
+}
 
 void RayPhysicsList::SetCuts() {
     SetCutsWithDefault();
@@ -29,7 +52,13 @@ void RayPhysicsList::SetCuts() {
 
 void RayPhysicsList::ConstructParticle()
 {
+
     G4OpticalPhoton::OpticalPhotonDefinition();
+
+    G4Gamma::GammaDefinition();
+    G4Electron::ElectronDefinition();
+    G4Positron::PositronDefinition();
+
 }
 
 void RayPhysicsList::ConstructProcess()
@@ -47,7 +76,7 @@ void RayPhysicsList::ConstructProcess()
 void RayPhysicsList::ConstructOpticalProcess()
 {
     G4OpAbsorption* theAbsProcess         = new G4OpAbsorption();
-    RayleighScattering* theRayProcess           = new RayleighScattering();
+    RayleighScattering* theRayProcess     = new RayleighScattering();
     //G4OpRayleigh* theRayProcess           = new G4OpRayleigh();
     G4OpBoundaryProcess* theBdProcess     = new G4OpBoundaryProcess();
     theBdProcess->SetInvokeSD(false);
